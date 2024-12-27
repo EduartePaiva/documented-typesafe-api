86ba2924f4fd9b64c88c2482120217de65e2086d
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2024-12-27 20:44:34 -0300

    creating a create-app function

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,7 @@
-import type { Env } from "hono";
-import type { PinoLogger } from "hono-pino";
+import createApp from "@/lib/create-app";
 
-import { OpenAPIHono } from "@hono/zod-openapi";
-import { logger } from "hono/logger";
-import { notFound, onError } from "stoker/middlewares";
+const app = createApp();
 
-import { pinoLogger } from "./middlewares/pino-logger";
-
-interface AppBindings {
-    Variables: {
-        logger: PinoLogger;
-    };
-}
-
-const app = new OpenAPIHono<AppBindings>();
-app.use(pinoLogger());
 app.get("/", (c) => {
     return c.text("Hello Hono!");
 });
@@ -24,8 +11,5 @@ app.get("/error", (c) => {
     c.var.logger.debug("only visible when debug is on");
     throw new Error("Oh no!");
 });
-
-app.notFound(notFound);
-app.onError(onError);
 
 export default app;
---
+++ src/lib/create-app.ts
@@ -0,0 +1,19 @@
+import { OpenAPIHono } from "@hono/zod-openapi";
+import { notFound, onError, serveEmojiFavicon } from "stoker/middlewares";
+
+import { pinoLogger } from "@/middlewares/pino-logger";
+
+import type { AppBindings } from "./types";
+
+function createApp() {
+    const app = new OpenAPIHono<AppBindings>({
+        strict: false,
+    });
+    app.use(serveEmojiFavicon("🧻"));
+    app.use(pinoLogger());
+    app.notFound(notFound);
+    app.onError(onError);
+    return app;
+}
+
+export default createApp;
